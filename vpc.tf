95e364df469e9e3f60917dcb4b60ed42508c9d36
gcrow <gee.crow@gmail.com>
2020-07-02 12:12:39 +0000

    n

--- vpc.tf
+++ vpc.tf
@@ -1,19 +1,38 @@
-module "vpc" {
-  source = "app.terraform.io/gcrow-training/vpc/aws"
-  version = "2.44.0"
+provider "aws" {
+  region = "eu-west-1"
+}
 
-  name = "myvpc"
+data "aws_security_group" "default" {
+  name   = "default"
+  vpc_id = module.vpc.vpc_id
+}
+
+module "vpc" {
+  source = "../../"
+
+  name = "simple-example"
+
   cidr = "10.0.0.0/16"
 
-  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  azs             = ["eu-west-1a", "eu-west-1b", "euw1-az3"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
+  enable_ipv6 = true
+
   enable_nat_gateway = true
-  enable_vpn_gateway = true
+  single_nat_gateway = true
+
+  public_subnet_tags = {
+    Name = "overridden-name-public"
+  }
 
   tags = {
-    Terraform = "true"
+    Owner       = "user"
     Environment = "dev"
+  }
+
+  vpc_tags = {
+    Name = "vpc-name"
   }
 }
